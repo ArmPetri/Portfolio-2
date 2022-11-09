1dd0d83763800ce97237f0651492a4a92eb38ccd
Armands Petrikas <armandspetrikas@gmail.com>
2022-11-09 21:36:41 +0200

    Parallax Test

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ for(let link of links) {
   })
 }
 
+// Parallax 
+document.addEventListener("mousemove", (e) => {
+  document.querySelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed')
+
+    const x = (window.innerWidth - e.pageX*speed)/100
+    const y = (window.innerHeight - e.pageY*speed)/100
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+  })
+});
+
+
 // Scroll Up 
 document.addEventListener("scroll", scrolling);
 
